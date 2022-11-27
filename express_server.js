251741448a557bf4ed6dedac0a98e6398dc3a611
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-27 22:07:45 +0000

    now can click on short url to take to long url and when entering new long url properly redirects to page showing long and short url

--- express_server.js
+++ express_server.js
@@ -11,13 +11,18 @@ let ranNum = function generateRandomString() {
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://wwww.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 //root page says hello
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+//adding /hello at the end shows Hello world with World in bold
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 //urls.json returns json string representing urlDatabase object
@@ -28,31 +33,37 @@ app.get("/urls.json", (req, res) => {
 //index is in views and provides format to return the urlDatabase object
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 })
 
 //route definition to add new long url and convert to short
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  res.render("urls_new");
 })
 
 //route takes in the user defined url and sends response of 6 random alphanumeric characters
 app.post("/urls", (req, res) => {
-  console.log(req.body)
-  res.send(ranNum());
+  let newLong = req.body;
+  let id = ranNum();
+  urlDatabase[id] = newLong["longURL"];
+  res.redirect(`/urls/${id}`);
 });
+
 
 //Found this in lecture to find longURL assign variable to re.params and then use urlDatabase to get key userInput 
 app.get("/urls/:id", (req, res) => {
-  let userInput = req.params.id
+  let userInput = req.params.id;
   const templateVars = { id:userInput, longURL:urlDatabase[userInput]};
-  res.render("urls_show", templateVars)
-})
-
-//adding /hello at the end shows Hello world with World in bold
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  res.render("urls_show", templateVars);
 });
+
+//can now click on short url to take you to long url.
+app.get("/u/:id", (req, res) => {
+  let userInput = req.params.id;
+  const longURL = urlDatabase[userInput]
+  res.redirect(longURL);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
