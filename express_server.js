f3921ae79afad82b204c3f296b1b07045a1cc289
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-12-04 00:04:39 +0000

    now with getUserByEmail function and error status if empty strings and user email already registered

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+//node modules
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -9,10 +10,14 @@ app.use(express.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
+
+//***helper functions
+
 //generates random 5 characters of letters and numbers
 let ranNum = function generateRandomString() {
   return Math.random().toString(36).slice(2,8);
 };
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,17 +25,26 @@ const urlDatabase = {
 };
 
 const users = {
-  userRandomID: {
+  'userRandomID': {
     id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
   },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
+  }
 };
+
+const getUserByEmail = function (email) {
+  for (let user in users) {
+    console.log(users[user])
+    if (users[user].email === email) {
+      return true;
+    }
+  }
+}
 
 //renders the urls_registration page
 app.get('/registration', (req, res) => {
@@ -40,27 +54,40 @@ app.get('/registration', (req, res) => {
 
 app.post('/registration', (req, res) => {
   const userID = ranNum();
-  users[userID] = {
-    userID,
-    email: req.body.email,
-    password: req.body.password
+  if(req.body.email === "" || req.body.password === "") {
+    //console.log(users)
+    res.status(400).send("email or password is empty")
+    res.redirect('/registration')
+  } else if (getUserByEmail(req.body.email)) {
+    res.status(400).send("user already exists")
+
+  } else {
+    users[userID] = {
+      userID,
+      email: req.body.email,
+      password: req.body.password
+    }
+    console.log(userID) //checking what this is
+    const user = users[userID].userID
+    res.cookie('users', user)
+    res.redirect('/urls')
   }
-  console.log(users[userID].userID)
-  console.log(users[userID])
-  cookie_id = res.cookie('users', users[userID].userID)
-  res.redirect('/urls')
+  //console.log(user)
+  
+  //console.log(users[userID]) 
 })
 
 //can enter username and deposits cookie to track username
 app.post('/login', (req, res) => {
-  const user = req.body.username;
-  res.cookie('user', user)
-  res.redirect('/urls')
+  //const user = Object.keys(users);
+  // console.log(user)
+  //res.cookie('users', user)
+  res.redirect('/registration')
 })
 
 app.post('/logout', (req, res) => {
   //clear the user cookie in this route
-  res.clearCookie('user')
+  res.clearCookie('users')
   res.redirect('/urls')
 })
 
@@ -81,14 +108,14 @@ app.get("/urls.json", (req, res) => {
 
 //urls_index is rendered, displays the urlDatabase object, displays the username if entered via cookie
 app.get("/urls", (req, res) => {
-  const user = req.cookies["user"]
+  const user = req.cookies["users"]
   const templateVars = {urls: urlDatabase, user};
   res.render("urls_index", templateVars);
 })
 
 //route definition to add new long url and convert to short, displays username via cookie in templateVars
 app.get("/urls/new", (req, res) => {
-  const templateVars = {user: req.cookies["user"]}
+  const templateVars = {user: req.cookies["users"]}
   res.render("urls_new", templateVars);
 })
 
